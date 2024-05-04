f3d526bcacc0ed2f47d03770360ede5d4844d5b1
mbuist <mbuist25@gmail.com>
2024-05-04 00:26:26 -0400

    added report functions

--- src/main/java/com/cs336/pkg/AdminUtil.java
+++ src/main/java/com/cs336/pkg/AdminUtil.java
@@ -3,6 +3,7 @@ package com.cs336.pkg;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
 
 /**
  * This class provides utility methods for admins and their permissions
@@ -171,6 +172,293 @@ public class AdminUtil {
             }
         }
         return false;
+    }
+    
+    /**
+     * generate report for total earnings
+     *
+     * @param void
+     * @return int of total earnings
+     */
+    public static int generateTotalEarnings () {
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+
+        try (Statement stmt = con.createStatement()) {
+            String insertQuery = "SELECT SUM(current_price) totalSum FROM sale s, shoes_auction sa WHERE s.shoes_id = sa.shoes_id";
+            PreparedStatement pstmt = con.prepareStatement(insertQuery);
+            ResultSet result = pstmt.executeQuery();
+            if (result.next()) { // Move cursor to the first row
+                return result.getInt("totalSum");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * generate report for total earnings
+     *
+     * @param void
+     * @return String[] arraylist of total earnings
+     */
+    public static ArrayList<String[]> getSales() {
+        ArrayList <String[]> sales = new ArrayList<String[]>();
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+
+        try (Statement stmt = con.createStatement()) {
+            String insertQuery = "SELECT * FROM sale s, shoes_auction sa WHERE s.shoes_id = sa.shoes_id ORDER BY sa.current_price DESC";
+            PreparedStatement pstmt = con.prepareStatement(insertQuery);
+            ResultSet result = pstmt.executeQuery();
+
+            while (result.next()) { // Move cursor to the first row
+                String[] saleDetails = new String[6];
+                saleDetails[0] = result.getString("shoes_id");
+                saleDetails[1] = result.getString("seller_username");
+                saleDetails[2] = result.getString("buyer_username");
+                saleDetails[3] = result.getString("name");
+                saleDetails[4] = result.getString("brand");
+                saleDetails[5] = result.getString("current_price");
+                sales.add(saleDetails);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sales;
+    }
+
+    /** 
+     * generate report for total earnings
+     *
+     * @param void
+     * @return String[] of the highest earning sale
+     */
+    public static ArrayList<String[]> getHighestEarning() {
+        ArrayList<String[]> highestEarningAL = new ArrayList<String[]>();
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+
+        try (Statement stmt = con.createStatement()) {
+            String insertQuery = "SELECT * FROM sale s, shoes_auction sa WHERE s.shoes_id = sa.shoes_id ORDER BY DESC";
+            PreparedStatement pstmt = con.prepareStatement(insertQuery);
+            ResultSet result = pstmt.executeQuery();
+
+            if (result.next()) { // Move cursor to the first row
+            	String[] highestEarning = new String[6];
+                highestEarning[0] = result.getString("shoes_id");
+                highestEarning[1] = result.getString("seller_username");
+                highestEarning[2] = result.getString("buyer_username");
+                highestEarning[3] = result.getString("name");
+                highestEarning[4] = result.getString("brand");
+                highestEarning[5] = result.getString("current_price");
+                highestEarningAL.add(highestEarning);                
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return highestEarningAL;
+    }
+
+    /**
+     * generate report for itemTypes
+     *
+     * @param itemtype
+     * @return String[] arraylist of that item type's sales
+     */
+    public static ArrayList<String[]> getItemTypeReports (String itemType) {
+        ArrayList<String[]> itemReport = new ArrayList<String[]> ();
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+        String insertQuery = "";
+
+        if (itemType.equals("boots")){
+            insertQuery = "SELECT * FROM sale s, boots_auction ba, shoes_auction sa WHERE s.shoes_id = ba.shoes_id AND s.shoes_id = sa.shoes_id";
+        }
+        else if (itemType.equals("sneakers")){
+            insertQuery = "SELECT * FROM sale s, sneakers_auction sna, shoes_auction sa WHERE s.shoes_id = sna.shoes_id AND s.shoes_id = sa.shoes_id";
+        }
+        else if (itemType.equals("sandals")){
+            insertQuery = "SELECT * FROM sale s, sandals_auction saa, shoes_auction sa WHERE s.shoes_id = saa.shoes_id AND s.shoes_id = sa.shoes_id";
+        }
+        else {
+            return null;
+        }
+
+        try (Statement stmt = con.createStatement()) {
+            PreparedStatement pstmt = con.prepareStatement(insertQuery);
+            ResultSet result = pstmt.executeQuery();
+
+            while (result.next()) { // Move cursor to the first row
+                String[] item = new String[6];
+                item[0] = result.getString("shoes_id");
+                item[1] = result.getString("seller_username");
+                item[2] = result.getString("buyer_username");
+                item[3] = result.getString("name");
+                item[4] = result.getString("brand");
+                item[5] = result.getString("current_price");
+                itemReport.add(item);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return itemReport;
+    }
+    
+    /**
+     * generate report for username
+     *
+     * @param username
+     * @return String[] arraylist of that username's sales
+     */
+    public static ArrayList<String[]> getUserReports (String username) {
+        ArrayList<String[]> itemReport = new ArrayList<String[]> ();
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+
+        try (Statement stmt = con.createStatement()) {
+            String insertQuery = " SELECT * FROM sale s, shoes_auction sa WHERE s.shoes_id = sa.shoes_id AND sa.seller_username = ?";
+            PreparedStatement pstmt = con.prepareStatement(insertQuery);
+            pstmt.setString(1, username);
+            ResultSet result = pstmt.executeQuery();
+
+            while (result.next()) { // Move cursor to the first row
+                String[] item = new String[6];
+                item[0] = result.getString("shoes_id");
+                item[1] = result.getString("seller_username");
+                item[2] = result.getString("buyer_username");
+                item[3] = result.getString("name");
+                item[4] = result.getString("brand");
+                item[5] = result.getString("current_price");
+                itemReport.add(item);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return itemReport;
+    }
+    
+    /**
+     * generate report for item type shoe name
+     *
+     * @param shoe name
+     * @return String[] arraylist of that shoe name sales
+     */
+    public static ArrayList<String[]> getShoeNameReport (String shoename) {
+        ArrayList<String[]> itemReport = new ArrayList<String[]> ();
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+
+        try (Statement stmt = con.createStatement()) {
+            String insertQuery = " SELECT * FROM sale s, shoes_auction sa WHERE s.shoes_id = sa.shoes_id AND sa.name = ?";
+            PreparedStatement pstmt = con.prepareStatement(insertQuery);
+            pstmt.setString(1, shoename);
+            ResultSet result = pstmt.executeQuery();
+
+            while (result.next()) { // Move cursor to the first row
+                String[] item = new String[6];
+                item[0] = result.getString("shoes_id");
+                item[1] = result.getString("seller_username");
+                item[2] = result.getString("buyer_username");
+                item[3] = result.getString("name");
+                item[4] = result.getString("brand");
+                item[5] = result.getString("current_price");
+                itemReport.add(item);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return itemReport;
+    }
+    
+    /**
+     * generate report for item type shoe name
+     *
+     * @param brand name
+     * @return String[] arraylist of that shoe name sales
+     */
+    public static ArrayList<String[]> getBrandNameReport (String brandname) {
+        ArrayList<String[]> itemReport = new ArrayList<String[]> ();
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+
+        try (Statement stmt = con.createStatement()) {
+            String insertQuery = " SELECT * FROM sale s, shoes_auction sa WHERE s.shoes_id = sa.shoes_id AND sa.brand = ?";
+            PreparedStatement pstmt = con.prepareStatement(insertQuery);
+            pstmt.setString(1, brandname);
+            ResultSet result = pstmt.executeQuery();
+
+            while (result.next()) { // Move cursor to the first row
+                String[] item = new String[6];
+                item[0] = result.getString("shoes_id");
+                item[1] = result.getString("seller_username");
+                item[2] = result.getString("buyer_username");
+                item[3] = result.getString("name");
+                item[4] = result.getString("brand");
+                item[5] = result.getString("current_price");
+                itemReport.add(item);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return itemReport;
     }
     
 }
